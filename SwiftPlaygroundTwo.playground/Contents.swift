4efdf137a4ebbe11709041405f8a3b970d33c3bb
Joseph Payongayong <cmstudent@uhwo-acm-020.local>
2020-02-04 09:56:37 -1000

    Completed parts 1-4 (04 Feb 2020)

---
+++ SwiftPlaygroundTwo.playground/Contents.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+var str = "Hello, playground"
+
+var goodGrade = "I've been doing all my work"
+if goodGrade == "I've been doing all my work"
+//if goodGrade == "I'm not doing very well"
+{
+    print("I should be getting an A at this point")
+}
+else
+{
+    print("I need to work harder in this class")
+}
+
+
+var gradeA = true
+if gradeA
+{
+    print("Wow! My first A in a coding class!")
+}
+else
+{
+    print("Coding is like learning a different language.")
+}
